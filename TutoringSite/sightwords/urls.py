91053772a25038db0ea0fe344b77e5f1234134d8
michael.connell <cage.connell@gmail.com>
2021-07-29 15:20:00 -0500

    navbar header has username at far right now.

--- TutoringSite/sightwords/urls.py
+++ TutoringSite/sightwords/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.SightwordIndexView.as_view()),
+    path('', views.SightwordIndexView.as_view(), name='Index'),
     path('Index', views.SightwordIndexView.as_view()),
     # path('', views.index),
     # path('Index', views.index),
