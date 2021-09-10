1c6e11375448ad9f14849f1a0592df965f32ed49
michael.connell <cage.connell@gmail.com>
2021-09-09 21:09:46 -0500

    Working on Sight Word sentences_and_words.html

--- TutoringSite/read_in_context/urls.py
+++ TutoringSite/read_in_context/urls.py
@@ -4,5 +4,4 @@ from . import views
 app_name = 'read_in_context'
 urlpatterns = [
     path('', views.SRAPassageIndexView.as_view(), name='Index'),
-    path('SRAPassage/<int:pk>', views.SRAPassageDetailView.as_view(), name='SRAPassage'),
 ]
