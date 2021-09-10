4723a348c893b0249737834cf7709d48e788732d
michael.connell <cage.connell@gmail.com>
2021-09-09 21:44:41 -0500

    Working on Sight Word sentences_and_words.html

--- TutoringSite/read_in_context/views.py
+++ TutoringSite/read_in_context/views.py
@@ -16,3 +16,15 @@ class SRAPassageIndexView(PermissionRequiredMixin, ListView):
         context = super().get_context_data(**kwargs)
         context['ref_tab'] = True
         return context
+
+
+class SRAPassageDetailView(PermissionRequiredMixin, DetailView):
+    permission_required = 'SRABook.view_SRABook'
+    template_name = "read_in_context/SRAPassagedetail.html"
+    model = SRAPassage
+    context_object_name = 'SRAPassage'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['ref_tab'] = True
+        return context
