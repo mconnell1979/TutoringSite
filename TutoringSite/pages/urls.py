24f45f2addd8e057227ed802993953c4f079a3cf
michael connell <cage.connell@gmail.com>
2022-05-01 10:08:30 -0500

    Organized the base with head and navbar includes

--- TutoringSite/pages/urls.py
+++ TutoringSite/pages/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from .views import IndexView
 from .views import ContactView
+from .views import WorkWithUsView
 from . import views
 
 app_name = 'pages'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('contact/', ContactView.as_view(), name='contact'),
+    path('workwithus/', WorkWithUsView.as_view(), name='workwithus'),
     path('empty', views.empty),
     ]
--- TutoringSite/pages/views.py
+++ TutoringSite/pages/views.py
@@ -23,3 +23,12 @@ class ContactView(TemplateView):
         context = {}
         context['contact_tab'] = True
         return context
+
+
+class WorkWithUsView(TemplateView):
+    template_name = "pages/work_with_us.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = {}
+        context['workwithus_tab'] = True
+        return context
