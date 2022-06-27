7bc44a5cc773cd72fb38fefc3644a19c4ba5489e
michael connell <cage.connell@gmail.com>
2022-06-27 18:21:50 -0500

    Added Unique Together For VV Stories

--- TutoringSite/vv_stories/admin.py
+++ TutoringSite/vv_stories/admin.py
@@ -15,7 +15,7 @@ class VVStoryQuestionInline(admin.StackedInline):
 
 class VVStoryAdmin(admin.ModelAdmin):
     inlines = [VVStoryQuestionInline]
-    list_display = ('id', 'book', 'number', 'title')
+    list_display = ('id', 'book', 'number', 'story_type', 'title')
 
 
 admin.site.register(VVStoryBook, VVStoryBookAdmin)
