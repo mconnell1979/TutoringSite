5d5dfdb6927da3e7fc87cf6cbeebe5bccf516728
michael connell <cage.connell@gmail.com>
2025-05-22 15:00:00 -0500

    some more DB record test changes for testing

--- TutoringSite/read_in_context/urls.py
+++ TutoringSite/read_in_context/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 app_name = 'read_in_context'
+
 urlpatterns = [
     path('', views.SRAPassageIndexView.as_view(), name='Index'),
+    path('<int:pk>/', views.SRAPassageDetailView.as_view(), name='SRAPassage'),  # 👈 this line is critical
 ]
--- TutoringSite/read_in_context/views.py
+++ TutoringSite/read_in_context/views.py
@@ -1,19 +1,22 @@
-from django.core.exceptions import ObjectDoesNotExist
-from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, DetailView
-from django.contrib.auth.mixins import PermissionRequiredMixin
 from .models import SRABook, SRAPassage
+from django.contrib.auth.mixins import PermissionRequiredMixin
 
-
+# existing index view
 class SRAPassageIndexView(PermissionRequiredMixin, ListView):
     permission_required = 'SRABook.view_SRABook'
     template_name = "read_in_context/index.html"
     model = SRAPassage
     context_object_name = 'SRAPassages'
-    queryset = SRAPassage.objects.all()
-    print("SRA Passage View Trying")
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['ref_tab'] = True
         return context
+
+# 👇 new detail view for individual passage
+class SRAPassageDetailView(PermissionRequiredMixin, DetailView):
+    permission_required = 'SRABook.view_SRABook'
+    template_name = "read_in_context/SRAPassagedetail.html"
+    model = SRAPassage
+    context_object_name = 'SRAPassage'
