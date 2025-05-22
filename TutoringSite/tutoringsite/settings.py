e37d4189b2276bede8c14e95b453a30b64128f9b
michael connell <cage.connell@gmail.com>
2025-05-21 23:05:44 -0500

    some more DB record test changes for testing

--- TutoringSite/tutoringsite/settings.py
+++ TutoringSite/tutoringsite/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-0+y_n^ua6dcae*+bfc@22(4f=swj46vpe@=8%dkijmgkr10ed^')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
